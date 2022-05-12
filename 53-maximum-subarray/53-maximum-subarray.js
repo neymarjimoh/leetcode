928d9e4b239aa6e2d2f3d73ea28d1a13063b6d4a
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-12 20:33:46 +0100

    Time: 122 ms (32.50%), Space: 50.1 MB (89.08%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum;
+};
