9fee1a72a3054a7d8a019cb4dba97010bc0faa64
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-15 20:54:18 +0100

    Time: 173 ms (86.66%), Space: 51.3 MB (38.59%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -7,14 +7,6 @@ var isPalindrome = function(x) {
         return false
     }
     
-    let reversed = 0 
-    let  rem = 0
-    let original = x
-    
-    while(x > 0) {
-        reversed = (reversed * 10) + (x % 10)
-        x = parseInt(x/10)
-    }
-    
-    return original == reversed  
+   const reversed = `${x}`.split("").reverse().join("");
+    return reversed == x;
 };
