583804cb590fbd7de7c115dbd77f7130e178f406
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-15 23:14:53 +0100

    Time: 77 ms (50.19%), Space: 41.9 MB (79.27%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for(let i = digits.length - 1 ; i >= 0 ; i--) {
+        if (digits[i] == 9) {
+            digits[i] = 0
+        } else {
+            digits[i] += 1
+            return digits
+        }
+    }
+
+    if(digits[0] == 0) {
+        digits.unshift(1)
+    }
+    return digits
+};
