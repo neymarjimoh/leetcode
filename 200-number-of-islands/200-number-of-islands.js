2a0f1d0eb02f400ce308dc8796157fb26d4946cb
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-18 22:38:22 +0100

    Time: 140 ms (33.55%), Space: 59.4 MB (11.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -2,21 +2,21 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
+var numIslands = function(grid, memo = {}) {
     const rows = grid.length;
     const cols = grid[0].length;
     let output = 0;
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             if (grid[i][j] === "1") {
-                dfs(grid, i, j);
+                dfs(grid, i, j, memo);
                 output += 1;
             }
         }
     }
     return output;
     
-    function dfs(grid, rowNum, colNum, memo = {}) {
+    function dfs(grid, rowNum, colNum, memo) {
         // make sure it is not out of scope of the grid (graph)
         if (rowNum < 0 || rowNum >= grid.length || colNum < 0 || colNum >= grid[0].length || grid[rowNum][colNum] == "0"
            || `r${rowNum}c${colNum}` in memo)         {
