f58ed952ffcb8ac6483979a28bf333f9417d6459
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-21 21:48:30 +0100

    Time: 89 ms (86.92%), Space: 51 MB (45.84%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set(nums);
+    const ans = set.size == nums.length
+    return !ans;
+};
