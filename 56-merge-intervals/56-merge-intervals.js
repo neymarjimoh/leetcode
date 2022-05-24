b3eb048a4c581b8f31f047ad528f4f5074895065
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-24 21:36:37 +0100

    Time: 123 ms (71.08%), Space: 48.3 MB (90.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -4,19 +4,15 @@
  */
 var merge = function(intervals) {
     if(!intervals.length) return [];
-    intervals.sort((a,b)=>a[0]-b[0]);
-
+    intervals = intervals.sort((a, b) => a[0] - b[0]);
     let res = [];
     let previous = intervals[0];
-
-    // merge
-    for(let i=1;i<intervals.length;i++) {
-        let current = intervals[i];
-        if(previous[1] >= current[0]) {
-            previous[1] = Math.max(previous[1],current[1]);
+    for (let i = 1; i < intervals.length; i++) {
+        if (previous[1] >= intervals[i][0]) {
+            previous[1] = Math.max(previous[1], intervals[i][1]);
         } else {
             res.push(previous);
-            previous = current;
+            previous = intervals[i];
         }
     }
     res.push(previous);
