a666edc4563870498d6e75c27c17630a4abbb99b
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-05-30 23:42:42 +0100

    Time: 131 ms (13.06%), Space: 45.1 MB (19.18%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -3,16 +3,15 @@
  * @return {boolean}
  */
 var divideArray = function(nums) {
-    if (nums.length % 2 !== 0) return false;
+    let obj = {};
     
-    const map = {};
-    
-    for (let num of nums) {
-        map[num] ? map[num]++ : map[num] = 1;
+    for (let i = 0; i < nums.length; i++) {
+      if (obj[nums[i]]) obj[nums[i]] += 1;
+      else obj[nums[i]] = 1;
     }
     
-    for (let key in map) {
-        if (map[key] % 2 !== 0)  return false;
+    for (let i of Object.values(obj)) {
+      if (i % 2 != 0) return false
     }
     
     return true;
