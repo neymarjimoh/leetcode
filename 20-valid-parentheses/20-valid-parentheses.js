b6b1e29daecdce7d60e4c70c31f40dfc48afea69
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-06-07 22:30:33 +0100

    Update 20-valid-parentheses.js

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,23 +3,16 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    var matches = {'(': ')', '{': '}', '[': ']'};
-	var stack = [];
-	var currentChar;
-
-	for (var i = 0; i < s.length; i++) {
-		currentChar = s[i];
-		if (matches[currentChar]) {
-			// opening braces
-			stack.push(currentChar);
-		} else {
-			// closing braces
-			if (currentChar !== matches[stack.pop()]) {
-				return false;
-			}
-		}
-	}
-
-	return stack.length === 0;
-
+    const matches = { "(" : ")", "{" : "}", "[" : "]" };
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in matches) {
+            stack.push(s[i]);
+        } else {
+            if (s[i] !== matches[stack.pop()]) {
+                return false;
+            }    
+        }
+    }
+    return stack.length === 0;
 };
