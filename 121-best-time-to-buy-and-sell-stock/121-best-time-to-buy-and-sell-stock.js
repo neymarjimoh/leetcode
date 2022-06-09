a9df2340bff12ddfe9365ad7cf9e62122b110dc1
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-06-09 22:10:48 +0100

    Update 121-best-time-to-buy-and-sell-stock.js

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,11 +4,30 @@
  */
 var maxProfit = function(prices) {    
     
-    let result = 0;
-    let min = prices[0];
-    for(let i = 1; i < prices.length; i++) {
-        min = Math.min(prices[i], min);
-        result = Math.max(result, prices[i] - min);
+//     let result = 0;
+//     let min = prices[0];
+//     for(let i = 1; i < prices.length; i++) {
+//         min = Math.min(prices[i], min);
+//         result = Math.max(result, prices[i] - min);
+//     }
+//     return result;
+    
+    // use https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735550/Python-Javascript-Easy-solution-with-very-clear-Explanation to understand better
+    // make prices[left] be buy and price[right] to sell
+    let left = 0;
+    let right = 0;
+    let max_profit = 0;
+    
+    // iterate as long as the right is not more than array length
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) {
+            const currProfit = prices[right] - prices[left];
+            max_profit = Math.max(max_profit, currProfit);
+        } else {
+            left = right;
+        }
+        right++
     }
-    return result;
+    
+    return max_profit;
 };
