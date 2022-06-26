f4079ffe00dde3603677a93176110cc6ae69e52e
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-06-26 21:13:42 +0100

    Time: 152 ms (6.63%), Space: 46.4 MB (6.27%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,18 +10,7 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-//     if (head == null) return false;
-//     const obj = {};
-    
-//     while (head) {
-//         if (head in obj) return true;
-//         obj[head] = 1;
-//         head = head.next;
-//     }
-    
-//     return false;
-    
+var hasCycle = function(head) {    
     let set = new Set();
     while (head) {
         if (set.has(head)) return true;
