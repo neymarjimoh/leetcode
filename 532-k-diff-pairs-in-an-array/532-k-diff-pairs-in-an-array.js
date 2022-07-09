1b8bebb97c38e9e997a8bf3457d613437776bdb7
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-07-09 12:35:02 +0100

    Time: 122 ms (44.35%), Space: 45.8 MB (70.16%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.js
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.js
@@ -4,17 +4,25 @@
  * @return {number}
  */
 var findPairs = function(nums, k) {
-    nums.sort((a, b) => b - a);
-    const set = new Set();
-    console.log(nums)
-    for (let i = 0; i < nums.length; i++) {
-        for (let j= i + 1; j < nums.length; j++) {
-            if (nums[i] - nums[j] == k) {
-                const val = [nums[j], nums[i]];
-                set.add(`${val}`)
-            }
-        }
-    }
-    console.log(set)
-    return set.size;
+    // nums.sort((a, b) => b - a);
+    // const set = new Set();
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j= i + 1; j < nums.length; j++) {
+    //         if (nums[i] - nums[j] == k) {
+    //             const val = [nums[j], nums[i]];
+    //             set.add(`${val}`)
+    //         }
+    //     }
+    // }
+    // return set.size;
+    
+    if (k < 0) return 0;
+  let store = new Set();
+  let result = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (store.has(nums[i] + k)) result.add(nums[i] + k);
+    if (store.has(nums[i] - k)) result.add(nums[i]);
+    store.add(nums[i]);
+  }
+  return result.size;
 };
