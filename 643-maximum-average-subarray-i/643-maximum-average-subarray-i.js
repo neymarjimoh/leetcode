b95458fe3aa0a3d3daa8762810ba6919147dd682
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-07-21 05:09:29 +0100

    Time: 171 ms (26.50%), Space: 55.2 MB (26.50%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -5,17 +5,31 @@
  */
 var findMaxAverage = function(nums, k) {
     // first approach
-    let sum = 0;
-    let maxAverage = -Infinity;
-    let windowStart = 0;
-    for (let i = 0; i < nums.length; i++) {
+    // let sum = 0;
+    // let maxAverage = -Infinity;
+    // let windowStart = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     sum += nums[i];
+    //     if (i >= k - 1) {
+    //         const average = sum / k;
+    //         maxAverage = Math.max(maxAverage, average);
+    //         sum -= nums[windowStart];
+    //         windowStart += 1;
+    //     }
+    // }
+    // return maxAverage;
+    
+    // second approach
+    let sum = 0,
+        maxSum = 0;
+    for(let i = 0; i < k; ++i) {
         sum += nums[i];
-        if (i >= k - 1) {
-            const average = sum / k;
-            maxAverage = Math.max(maxAverage, average);
-            sum -= nums[windowStart];
-            windowStart += 1;
-        }
     }
-    return maxAverage;
+    maxSum = sum;
+    
+    for(let i = k; i < nums.length; ++i) {
+        sum = sum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, sum);
+    }
+    return maxSum / k;
 };
