d1a01017c32572f24cf5f9fd73340e7ec4797878
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2022-08-14 08:41:48 +0100

    Time: 92 ms (93.73%), Space: 44.8 MB (92.57%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let start = 0, maxLength = 0;
+    let maxLength = 0;
+    let start = 0;
     const map = new Map();
     for (let i = 0; i < s.length; i++) {
         const currentChar = s[i];
         if (map.get(currentChar) >= start) {
             start = map.get(currentChar) + 1;
         }
-        map.set(currentChar, i);
-        maxLength = Math.max(maxLength, i - start + 1);
+        map.set(currentChar, i)
+        maxLength = Math.max(maxLength, i -start + 1);
     }
-    
     return maxLength;
 };
