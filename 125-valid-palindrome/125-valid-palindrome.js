9e5bf1bbc8feab8a039aae2d917886a14be698aa
Jimoh Rildwan Adekunle <jemohkunle2007@gmail.com>
2023-03-15 19:11:39 +0100

    Update 125-valid-palindrome.js

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,23 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-  s = s.toLowerCase().replace(/[^0-9a-z]/gi, "");
-
-  // using while loop
-  let left = 0,
-    right = s.length - 1;
-
-  while (left < right) {
-    if (s[left] !== s[right]) return false;
-    left++;
-    right--;
-  }
-  return true;
-
-  // using for loop
-  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
-    if (s[i] !== s[j]) return false;
-  }
-  return true;
+var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        if (!isAlphaNumeric(s[left])) {
+            left++;
+        } else if (!isAlphaNumeric(s[right])) {
+            right--;
+        } else {
+            if (s[left] !== s[right]) return false;
+            left++;
+            right--;
+        }
+    }
+    return true;
 };
+
+function isAlphaNumeric(char) {
+    // note: numbers have ascii code between 48 and 57
+    // uppercase has between 65 and 90
+    // lowercase has between 97 and 122
+    const charCode = char.charCodeAt(0);
+    return (charCode > 47 && charCode < 58) || (charCode > 96 && charCode < 123);
+}
